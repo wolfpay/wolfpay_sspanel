909d83a7f3b0ad3489ef8db28907aae459f25a3f
黃柏澄(風雨) <wartw@users.noreply.github.com>
2020-02-18 19:36:44 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
-$System_Config['payment_system']='wolfpay';
+$System_Config['payment_system_A']='wolfpay';
+$System_Config['payment_system_B']='wolfpay';
+$System_Config['payment_system_C']='wolfpay';
 //需修改支付接口
 $System_Config['wolfpay']=['config'=>['hid'=>'商户ID','key'=>'商户KEY','url'=>'商户API网址']];
 //以上内容可以在用户中心首页下方取得
